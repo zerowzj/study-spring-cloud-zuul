279f68af186d6966d232843486612229ba48d559
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-09-18 16:31:11 +0800

    update code

---
+++ src/main/java/study/springcloud/zuul/controller/ErrorHandlerController.java
@@ -0,0 +1,28 @@
+package study.springcloud.zuul.controller;
+
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class ErrorHandlerController implements ErrorController {
+
+    private static final String ERROR_PATH = "/error";
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+    @RequestMapping(ERROR_PATH)
+    public Object error(HttpServletRequest request, HttpServletResponse response) {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ZuulException exception = (ZuulException) ctx.getThrowable();
+        return Result.choose(exception.nStatusCode, exception.getMessage());
+    }
+}
--- src/main/java/study/springcloud/zuul/support/config/SpringBootCfg.java
+++ src/main/java/study/springcloud/zuul/support/config/SpringBootCfg.java
@@ -1,8 +1,10 @@
 package study.springcloud.zuul.support.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@ServletComponentScan(basePackages = "study.springcloud.zuul")
 @EnableZuulProxy
 @SpringBootApplication(scanBasePackages = "study.springcloud.zuul")
 public class SpringBootCfg {
