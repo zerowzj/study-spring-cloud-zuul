9e93da5df865ecfabe301533fb673809471681d6
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-04 15:45:55 +0800

    更新代码

--- src/main/java/study/springcloud/zuul/support/filter/AuthFilter.java
+++ src/main/java/study/springcloud/zuul/support/filter/AuthFilter.java
@@ -1,11 +1,26 @@
 package study.springcloud.zuul.support.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.bouncycastle.asn1.ocsp.ResponseData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
 public class AuthFilter extends ZuulFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthFilter.class);
 
     @Override
     public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.setSendZuulResponse(false);
+        ctx.setResponseBody("sadfasdf");
+        ctx.getResponse().setContentType("application/json; charset=utf-8");
         return false;
     }
 
@@ -16,7 +31,7 @@ public class AuthFilter extends ZuulFilter {
 
     @Override
     public String filterType() {
-        return null;
+        return "pre";
     }
 
     @Override
