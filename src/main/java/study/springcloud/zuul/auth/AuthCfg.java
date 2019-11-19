1dedab0e4ed774917da88f2c4e68b54b39794d5a
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-19 17:13:44 +0800

    更新代码

--- src/main/java/study/springcloud/zuul/auth/AuthCfg.java
+++ src/main/java/study/springcloud/zuul/auth/AuthCfg.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 @EnableOAuth2Sso
 public class AuthCfg extends WebSecurityConfigurerAdapter {
 
