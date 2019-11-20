bd827e5390dab42bd4ad4c0344e0d5e625abac03
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-20 09:18:40 +0800

    提交代码

--- src/main/java/study/springcloud/zuul/auth/AuthCfg.java
+++ src/main/java/study/springcloud/zuul/auth/AuthCfg.java
@@ -1,12 +1,13 @@
 package study.springcloud.zuul.auth;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableOAuth2Sso
 public class AuthCfg extends WebSecurityConfigurerAdapter {
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
