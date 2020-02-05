b7b5dd2c3c870415cb4513f6554541c3376b8c3b
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-05 22:58:26 +0800

    更新代码

--- src/main/java/study/springcloud/zuul/filter/WatchDogFilter.java
+++ src/main/java/study/springcloud/zuul/filter/WatchDogFilter.java
@@ -5,11 +5,9 @@ import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Component
