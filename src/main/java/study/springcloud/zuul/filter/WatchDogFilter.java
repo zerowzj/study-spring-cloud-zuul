ce72df93f728d686b8d7b5e9837d4ff71f25a64d
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-05 21:27:42 +0800

    更新代码

--- src/main/java/study/springcloud/zuul/filter/WatchDogFilter.java
+++ src/main/java/study/springcloud/zuul/filter/WatchDogFilter.java
@@ -1,25 +1,40 @@
 package study.springcloud.zuul.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
 public class WatchDogFilter extends ZuulFilter {
 
     @Override
+    public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String uri = request.getRequestURI();
+        log.info("{}", uri);
+        return true;
+    }
+
+    @Override
     public String filterType() {
-        return FilterConstants.POST_TYPE;
+        return FilterConstants.PRE_TYPE;
     }
 
     @Override
     public int filterOrder() {
-        return 0;
+        return -1;
     }
 
-    @Override
-    public boolean shouldFilter() {
-        return true;
-    }
+
 
     @Override
     public Object run() throws ZuulException {
