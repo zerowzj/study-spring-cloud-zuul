597013263076af2725867b71725adfeda48d8f83
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-05 18:22:31 +0800

    更新代码

--- src/main/java/study/springcloud/zuul/auth/AuthCfg.java
+++ src/main/java/study/springcloud/zuul/auth/AuthCfg.java
@@ -1,23 +1,23 @@
-package study.springcloud.zuul.auth;
-
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableOAuth2Sso
-public class AuthCfg extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/login", "/client/**", "/auth/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .csrf()
-                .disable();
-    }
-}
+//package study.springcloud.zuul.auth;
+//
+//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableOAuth2Sso
+//public class AuthCfg extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/login", "/client/**", "/auth/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .csrf()
+//                .disable();
+//    }
+//}
--- src/main/java/study/springcloud/zuul/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/zuul/support/SpringBootCfg.java
@@ -1,8 +1,10 @@
 package study.springcloud.zuul.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableHystrixDashboard
 @EnableZuulProxy
 //@ServletComponentScan(basePackages = "study.springcloud.zuul")
 @SpringBootApplication(scanBasePackages = "study.springcloud.zuul")
