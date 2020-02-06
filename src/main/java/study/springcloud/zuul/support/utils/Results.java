0e6c70e0dacad2d6991e93a0875329d11e76351c
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-06 16:17:45 +0800

    更新代码

---
+++ src/main/java/study/springcloud/zuul/support/utils/Results.java
@@ -0,0 +1,10 @@
+package study.springcloud.zuul.support.utils;
+
+import com.sun.javafx.collections.MappingChange;
+
+public class Results {
+
+    public static MappingChange.Map<String, Object> ok(){
+        return null;
+    }
+}
