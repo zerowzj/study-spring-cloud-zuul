b96161350d267496672fec6d5380357347ad0336
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-08 18:35:28 +0800

    更新代码

--- src/main/java/study/springcloud/zuul/controller/GlobalErrorController.java
+++ src/main/java/study/springcloud/zuul/controller/GlobalErrorController.java
@@ -11,6 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class GlobalErrorController implements ErrorController {
 
+    private static final String KEY_STATUS_CODE = "javax.servlet.error.status_code";
+
+    private static final String KEY_EXCEPTION = "javax.servlet.error.exception";
+
+    private static final String KEY_MESSAGE = "javax.servlet.error.message";
+
     @Override
     public String getErrorPath() {
         return "/error";
@@ -20,10 +26,10 @@ public class GlobalErrorController implements ErrorController {
     public String error(HttpServletRequest request) {
         String uri = request.getRequestURI();
         log.info("uri={}", uri);
-        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        Integer statusCode = (Integer) request.getAttribute(KEY_STATUS_CODE);
         log.info("{}", statusCode);
-        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
-        String message = (String) request.getAttribute("javax.servlet.error.message");
+        Exception ex = (Exception) request.getAttribute(KEY_EXCEPTION);
+        String msg = (String) request.getAttribute(KEY_MESSAGE);
         return "faaaaaaaaa";
     }
 }
