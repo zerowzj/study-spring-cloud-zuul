f60b547b34ffd36234f5cbf688959b4d7f9ce433
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-09 16:01:11 +0800

    更新代码

--- src/main/java/study/springcloud/zuul/support/exception/GlobalExceptionHandler.java
+++ src/main/java/study/springcloud/zuul/support/exception/GlobalExceptionHandler.java
@@ -1,21 +1,21 @@
-//package study.springcloud.zuul.support.exception;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@Slf4j
-//@RestControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//    @ExceptionHandler(Throwable.class)
-//    public Map<String, Object> handle(Throwable ex) {
-//        Map<String, Object> result = new HashMap<>();
-//        result.put("code", "9999");
-//        result.put("desc", ex.getMessage());
-//        return result;
-//    }
-//}
+package study.springcloud.zuul.support.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Throwable.class)
+    public Map<String, Object> handle(Throwable ex) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("code", "8888");
+        result.put("desc", ex.getMessage());
+        return result;
+    }
+}
