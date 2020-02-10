184cc3a2f269bbcece0bb5a2fcb457c65ae4ce4f
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-10 17:13:25 +0800

    更新代码

--- src/main/java/study/springcloud/zuul/controller/DemoController.java
+++ src/main/java/study/springcloud/zuul/controller/DemoController.java
@@ -1,21 +1,13 @@
 package study.springcloud.zuul.controller;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.TimeUnit;
 
 @RestController
 public class DemoController {
 
     @RequestMapping("/demo")
     public String demo() {
-        try {
-            TimeUnit.SECONDS.sleep(10);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
         return "this is demo";
     }
 }
