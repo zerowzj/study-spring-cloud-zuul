df2183f7eeed9262bc5669689b3fc67c5a2d6753
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-13 01:02:32 +0800

    更新代码

--- src/main/java/study/springcloud/zuul/support/fallback/GlobalFallbackProvider.java
+++ src/main/java/study/springcloud/zuul/support/fallback/GlobalFallbackProvider.java
@@ -11,11 +11,10 @@ public class GlobalFallbackProvider implements FallbackProvider {
 
     /**
      * 返回值表示需要针对此微服务做回退处理
-     * 该名称一定要是注册进入 eureka 微服务中的那个 serviceId 名称
      */
     @Override
     public String getRoute() {
-        //服务id，如果需要所有调用都支持回退，则return "*"或return null
+        //serviceId，如果需要所有调用都支持回退，则return "*"或return null
         return "*";
     }
 
