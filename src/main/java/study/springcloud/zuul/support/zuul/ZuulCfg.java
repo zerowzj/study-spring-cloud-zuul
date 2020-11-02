e4e725b0d253b8d29fea2210a64a145bb847edf5
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-02 17:47:19 +0800

    更新代码

--- src/main/java/study/springcloud/zuul/support/zuul/ZuulCfg.java
+++ src/main/java/study/springcloud/zuul/support/zuul/ZuulCfg.java
@@ -1,4 +1,4 @@
-package study.springcloud.zuul.support;
+package study.springcloud.zuul.support.zuul;
 
 import org.springframework.cloud.netflix.zuul.filters.post.LocationRewriteFilter;
 import org.springframework.context.annotation.Bean;
